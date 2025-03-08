94ff77cd22b15f710491db9f152b39d52335afdd
Alejandro Peña <ja.penat1@uniandes.edu.co>
2025-03-08 12:39:33 -0500

    Add button to synchronize patient

--- src/app/components/patient-info/patient-info.component.ts
+++ src/app/components/patient-info/patient-info.component.ts
@@ -1,17 +1,32 @@
 import { Component, Input } from '@angular/core';
 import { Patient } from '../../models/patient.model';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-patient-info',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatSnackBarModule],
   templateUrl: './patient-info.component.html',
   styleUrl: './patient-info.component.scss',
 })
 export class PatientInfoComponent {
   @Input() selectedPatient: Patient | null = null;
 
+  constructor(private snackBar: MatSnackBar) {}
+
   onSyncPatient(): void {
-    console.debug('Syncing patient data...');
+    if (!this.selectedPatient) return;
+
+    this.snackBar.open(
+      `Sincronización exitosa con la aplicación de ${this.selectedPatient.name}`,
+      'OK',
+      {
+        duration: 3000,
+        horizontalPosition: 'center',
+        verticalPosition: 'bottom',
+        panelClass: ['success-snackbar'],
+      }
+    );
   }
 }
